3dca472a066b102cac100e948848f646b5ee3cfb
AthanasiusOfAlex <lmelahn@legionaries.org>
2025-11-16 09:35:37 +0100

    Created XCode project

---
+++ QtiEditor/QtiEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QtiEditorApp.swift
+//  QtiEditor
+//
+//  Created by Louis Melahn on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct QtiEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
